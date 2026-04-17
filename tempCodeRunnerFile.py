ec83a0bf980fc022b75899f20d0421bbe95a608b
Atif Bashir <atifparay16@gmail.com>
2026-04-17 19:49:12 +0530

    numpy

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
-
 import numpy as np
-Atif = np.array([1, 2, 3, 4, 5], ndmin=5)
-print(Atif)
-print('number of dimension: ', Atif.ndim)
 
+Atif = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]])
+print(Atif[0, 1, 2])
