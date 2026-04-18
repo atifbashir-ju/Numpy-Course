c1f9885e87fe7f57ff4fca6a9a2cce98e2bd7f5a
Atif Bashir <atifparay16@gmail.com>
2026-04-18 19:29:15 +0530

    random number

---
+++ numpy_randomnumber.py
@@ -0,0 +1,26 @@
+import numpy as np
+from numpy import random
+Atif = random.randint(100)
+print(Atif)
+
+
+from numpy import random
+Atif = random.rand()
+print(Atif)
+
+
+from numpy import random
+Atif = random.randint(100, size=(5))
+print(Atif)
+
+from numpy import random
+Atif = random.randint(100, size=(3, 5))
+print(Atif)
+
+from numpy import random
+Atif = random.choice([1, 2, 3, 4, 5])
+print(Atif)
+
+from numpy import random
+Atif = random.choice([1, 2, 3, 4,5, 6], size = (3, 5))
+print(Atif)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,3 @@
-import numpy as np
-Atif = np.array([41, 42, 43, 44])
-Atifempty = Atif > 42
-Atifnew = Atif[Atifempty]
-print(Atifempty)
-print(Atifnew)
+from numpy import random
+Atif = random.choice([1, 2, 3, 4,5, 6], size = (3, 5))
+print(Atif)
