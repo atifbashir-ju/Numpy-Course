391be5a75b602c1d5b832b8954c0853e34ae5722
Atif Bashir <atifparay16@gmail.com>
2026-04-18 15:40:37 +0530

    spliting

---
+++ numpy_siplitingarray.py
@@ -0,0 +1,31 @@
+#sipliting
+import numpy as np
+Atif = np.array([1, 2, 3, 4, 5, 6])
+Atifnew = np.array_split(Atif, 3)
+print(Atifnew)
+
+import numpy as np
+Atif = np.array([1, 2, 3, 4, 5, 6])
+Atifnew = np.array_split(Atif, 4)
+print(Atifnew)
+
+#split into array witih index
+import numpy as np
+Atif = np.array([1, 2, 3, 4, 5, 6])
+Atifnew = np.array_split(Atif, 3)
+print(Atifnew[0])
+print(Atifnew[1])
+print(Atifnew[2])
+
+
+#sipliting 2-D array
+import numpy as np
+Atif = np.array([[1, 2],[3, 4], [5, 6], [7, 8], [9, 10], [11,12]])
+Atifnew = np.array_split(Atif, 3)
+print(Atifnew)
+
+#hsplit()
+import numpy as np
+Atif = np.array([[1, 2],[3, 4], [5, 6], [7, 8], [9, 10], [11,12]])
+Atifnew = np.hsplit(Atif,2)
+print(Atifnew)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
 import numpy as np
-Atif = np.array([1, 2, 3])
-Atif1 = np.array([4, 5, 6])
-Atif2 = np.dstack((Atif, Atif1))
-print(Atif2)
-
+Atif = np.array([[1, 2],[3, 4], [5, 6], [7, 8], [9, 10], [11,12]])
+Atifnew = np.hsplit(Atif,3)
+print(Atifnew)
