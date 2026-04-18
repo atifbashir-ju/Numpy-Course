9e61f03104afe7da52613fd7efa6f6a8797ee44e
Atif Bashir <atifparay16@gmail.com>
2026-04-18 16:08:05 +0530

    searching Arrays

---
+++ numpyseacrhing_arrays.py
@@ -0,0 +1,28 @@
+#searching Array:
+import numpy as np
+Atif = np.array([1, 2, 3, 4, 5, 4, 4])
+Atifnew = np.where(Atif ==4)
+print(Atifnew)
+
+#find indexs where the value are even
+import numpy as np
+Atif = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+Atifnew = np.where(Atif % 2 == 0)
+print(Atifnew)
+
+#odd number
+import numpy as np
+Atif = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+Atifnew = np.where(Atif % 2 == 1)
+print(Atifnew)
+
+#searchshorted()
+import numpy as np
+Atif = np.array([6, 7, 8, 9])
+Atifnew = np.searchsorted(Atif, 9)
+print(Atifnew)
+
+import numpy as np
+Atif = np.array([6, 7, 8, 9])
+Atifnew = np.searchsorted(Atif, 7, side = 'right')
+print(Atifnew)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,4 @@
 import numpy as np
-Atif = np.array([[1, 2],[3, 4], [5, 6], [7, 8], [9, 10], [11,12]])
-Atifnew = np.hsplit(Atif,3)
+Atif = np.array([1, 3, 5, 7])
+Atifnew = np.searchsorted(Atif, [2,4,6])
 print(Atifnew)
