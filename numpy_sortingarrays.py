cda175ac7aeeaed697df4bb0815be53698deecd3
Atif Bashir <atifparay16@gmail.com>
2026-04-18 16:31:31 +0530

    sorting

---
+++ numpy_sortingarrays.py
@@ -0,0 +1,15 @@
+# Sort()
+import numpy as np
+Atif = np.array([3, 2, 0, 1])
+print(np.sort(Atif))
+
+
+import numpy as np
+Atif = np.array(['Banana' , 'Apple', 'Cat'])
+print(np.sort(Atif))
+
+#2-D Array sorting
+# Sort()
+import numpy as np
+Atif = np.array([[8, 4], [2, 1]])
+print(np.sort(Atif))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,3 @@
 import numpy as np
-Atif = np.array([1, 3, 5, 7])
-Atifnew = np.searchsorted(Atif, [2,4,6])
-print(Atifnew)
+Atif = np.array([[8, 4], [2, 1]])
+print(np.sort(Atif))
