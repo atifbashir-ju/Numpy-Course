7d994d21fad2211f22b7f0359e0240c99f51e50c
Atif Bashir <atifparay16@gmail.com>
2026-04-20 18:07:01 +0530

    rounding decimals

---
+++ numpy_roundingdecimal.py
@@ -0,0 +1,24 @@
+#Rounding Decimals 5 ways of rounding off:
+#1 truncation:
+
+import numpy as np
+Atif = np.trunc([-3.666, 3.6667]) 
+print(Atif)
+
+
+import numpy as np
+Atif = np.fix([-3.666, 3.6667]) 
+print(Atif)
+
+import numpy as np
+Atif = np.round([-3.666, 3.6667]) 
+print(Atif)
+
+import numpy as np
+Atif = np.floor([-3.666, 3.6667]) 
+print(Atif)
+
+import numpy as np
+Atif = np.ceil([-3.666, 3.6667]) 
+print(Atif)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
 import numpy as np
-Atif = np.array([10, 11, 12, 13, 14, 15])
-Atif1 = np.array([20, 21, 22, 23, 24, 25])
-Atifnew = np.power(Atif, Atif1)
-print(Atifnew)
+Atif = np.ceil([-3.666, 3.6667]) 
+print(Atif)
 
